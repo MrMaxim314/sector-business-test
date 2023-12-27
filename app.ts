19377222096927e58563358d1ee627885f130c68
MrMaxim314 <makson199910@gmail.com>
2023-12-28 01:29:20 +0300

    setup swagger

--- app.ts
+++ app.ts
@@ -1,9 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerUi, { SwaggerOptions } from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options: SwaggerOptions = {
+  definition: {
+    info: {
+      title: 'Test Task API',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000/',
+    },
+  ],
+  apis: [],
+};
 
 dotenv.config();
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 
 app.get('/ping', (req, res) => res.send('Ok'));
 
